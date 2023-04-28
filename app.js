27dde25fbc0b6211bd2ee474607a28d681d7474e
IvanMiyasoied <121316974+IvanMiyasoied@users.noreply.github.com>
2023-04-28 23:06:17 +0300

    start

---
+++ app.js
@@ -0,0 +1,28 @@
+// squareNum = number => number * number;
+
+// console.log(squareNum(4));
+
+
+
+
+// name = 'Mark'
+// surname = 'Spencer'
+
+// userName = (name,surname) => 
+//      name + " " + surname;
+
+// alert(userName(name,surname));
+
+
+
+
+// function showArgs () {
+//     args = Array.from(arguments)
+//     return args
+// }
+
+// showArgs(1,2,3,4,5,6,7,8,9,10);
+// console.log(args);
+
+
+
