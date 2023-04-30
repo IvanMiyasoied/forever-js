798982acfd92ea9bbe9c2dcb905a2cd60e2e50d6
IvanMiyasoied <121316974+IvanMiyasoied@users.noreply.github.com>
2023-05-01 00:21:03 +0300

    another one done

--- app.js
+++ app.js
@@ -26,3 +26,70 @@
 
 
 
+// let str = "ad;lkf;'aldk';;'lak'fD"
+
+// function findD(str) {
+//    result = str.indexOf('D');
+//    return result
+// }
+
+// findD(str);
+// console.log(result);
+
+
+
+// let strHello = "Hello world"
+
+//  sayHi = () => alert(strHello);
+ 
+//  sayHi();
+
+
+//  let a = 5
+//  let b = 7
+
+//  sumCase(a,b);  // error < canniot
+
+// let sumCase = function(a,b) {
+//     return a + b
+//  }
+
+// //  sumCase(a,b);  
+//  console.log(sumCase(a,b));
+
+
+// let double = n => n*2 ;
+// let sayHi = () => alert('Hello')
+// let sum = (a,b) => {
+//     let result = a + b;
+//     return result
+// };
+
+
+// function sumCaseTwo(a,b) {
+//     return a + b
+// }
+
+// let sum = function(a,b) {
+//     return a + b
+// } 
+
+
+// let checkNum = (number) => {
+//     if (number > 10) {
+//         alert ('true')
+//     } else {
+//         alert('false')
+//     }
+// }
+
+// checkNum(25);
+// console.log(checkNum(25));
+
+sumCaseThree(3,2)
+
+function sumCaseThree(a,b) {
+    return a + b
+}
+
+console.log(sumCaseThree(3,2));
